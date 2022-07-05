6be1dd837ac154275ccb5e9990041b017a913e52
matuakira <108610776+matuakira@users.noreply.github.com>
2022-07-06 00:17:13 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値取得
+  const inputText = document.getElementById("add-text").value;
+
+  // DOM操作・・・面倒くさい。
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.className = "list-row";
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // Completeボタン生成。ここでイベントリスナも設定する。
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "Complete";
+  // イベントリスナ
+  completeButton.addEventListener("click", () => {
+    alert("Complete");
+  });
+
+  // Deleteボタン生成。ここでイベントリスナも設定する。
+  const deleteButton = document.createElement("button");
+  // イベントリスナ
+  deleteButton.addEventListener("click", () => {
+    //　押された削除ボタンの親の親の親(li)を未完了リストから削除。
+    const deleteTarget = deleteButton.parentNode.parentNode.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+  deleteButton.innerText = "Delete";
+
+  // pタグ配下に置く
+  p.appendChild(completeButton);
+  p.appendChild(deleteButton);
+
+  //要素紐づけ
+  div.appendChild(p);
+  li.appendChild(div);
+
+  // 未完了TODOリストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+
+  // テキストボックス初期化
+  document.getElementById("add-text").value = "";
+};
+
+// イベント付与
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
