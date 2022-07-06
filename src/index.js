838c5fdc7066aaae7e3718684c5c8d1c63b01219
matuakira <108610776+matuakira@users.noreply.github.com>
2022-07-06 20:48:27 +0900

    update

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const onClickAdd = () => {
   const div = document.createElement("div");
   div.className = "list-row";
   const p = document.createElement("p");
+  const div2 = document.createElement("div");
+  p.appendChild(div2);
   p.innerText = inputText;
 
   // Completeボタン生成。ここでイベントリスナも設定する。
@@ -16,7 +18,39 @@ const onClickAdd = () => {
   completeButton.innerText = "Complete";
   // イベントリスナ
   completeButton.addEventListener("click", () => {
-    alert("Complete");
+    // ボタンが押された行を特定する。
+    const completeTarget = completeButton.parentNode.parentNode.parentNode;
+
+    // ボタンが押された行のTODOの文字列を取得する。
+    const todoString =
+      completeButton.parentNode.parentNode.firstElementChild.innerText;
+    console.log(todoString);
+
+    // 追加のための生成
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    div.className = "list-row";
+    const p = document.createElement("p");
+    p.innerText = todoString;
+
+    // Restoreボタン生成。イベントリスナも設定する。
+    const restoreButton = document.createElement("button");
+    restoreButton.innerText = "Restore";
+    //イベントリスナ
+    restoreButton.addEventListener("click", () => {
+      alert("restore!");
+    });
+
+    // Restoreボタンをpタグ配下に置く
+    p.appendChild(restoreButton);
+    //要素紐づけ
+    div.appendChild(p);
+    li.appendChild(div);
+
+    // CompletedなTODOの末尾に現在の行を追加する
+    document.getElementById("complete-list").appendChild(li);
+    // IncompletedなTODOリストから、ボタンが押された行を削除する。
+    deleteFromIncompleteList(completeTarget);
   });
 
   // Deleteボタン生成。ここでイベントリスナも設定する。
@@ -25,7 +59,7 @@ const onClickAdd = () => {
   deleteButton.addEventListener("click", () => {
     //　押された削除ボタンの親の親の親(li)を未完了リスト(ul)から削除。
     const deleteTarget = deleteButton.parentNode.parentNode.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromIncompleteList(deleteTarget);
   });
   deleteButton.innerText = "Delete";
 
@@ -36,12 +70,17 @@ const onClickAdd = () => {
   //要素紐づけ
   div.appendChild(p);
   li.appendChild(div);
-
+  console.log(li);
   // 未完了TODOリストに追加
   document.getElementById("incomplete-list").appendChild(li);
 
   // テキストボックス初期化
   document.getElementById("add-text").value = "";
+};
+
+// 未完了リストから指定ノードを削除する
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 // イベント付与
